593859c0614d89970f509f873500a1eb65156891
mukul jha <mukuljha302@gmail.com>
2022-04-06 00:02:56 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/starter_ui_socialmediaplatform/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.starter_ui_socialmediaplatform
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Pages/homescreen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+    Row(
+mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("Data",style: TextStyle(color: Colors.white)),
+//Image.asset("assets\images\i1.png",height: 50,width: 50,)
+        
+      ],
+    )
+
+
+  ],
+),
+
+
+
+
+    );
+
+
+  }
+}
