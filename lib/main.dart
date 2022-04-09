74a12e501c34b0c247532eb87d7ed2beda00bcd9
mukul jha <mukuljha302@gmail.com>
2022-04-09 17:28:00 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:starter_ui_socialmediaplatform/responsive/mobile_scrren_layout.dart';
 import 'package:starter_ui_socialmediaplatform/responsive/responsive_layout_screen.dart';
@@ -6,7 +7,9 @@ import 'package:starter_ui_socialmediaplatform/utils/colors/colors.dart';
 
 import 'Pages/homescreen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- lib/responsive/mobile_scrren_layout.dart
+++ lib/responsive/mobile_scrren_layout.dart
@@ -7,7 +7,83 @@ class MobileScreenLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("This is Mobile")),
+      body: Column(
+        //mainAxisAlignment: MainAxisAlignment.center,
+
+        children: [
+          SizedBox(
+            height:70,
+            width: double.infinity,
+          ),
+
+          Row(
+            children: [
+
+              Container(
+                margin: EdgeInsets.only(left: 20,right: 10),
+                 height: 100,
+                 width: 100,
+                   child:Column(
+                     crossAxisAlignment: CrossAxisAlignment.end,
+                     children: [
+                     Image.asset("assets/images/image8.jpg",height: 85,width: 100,),
+                     Container(
+                       height: 15,
+                       width: 15,
+                       decoration: BoxDecoration(
+                         color: Colors.red
+                             ,borderRadius: BorderRadius.circular(30)
+                       ),
+                     )
+
+                   ],)
+                   ,
+                //child: Image.asset("assets/images/image6.jpg"),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+
+                  borderRadius: BorderRadius.circular(90.0),
+
+                ),
+
+
+              )
+              ,
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Sojan Islam",style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+
+                    ),),
+                    Text("@sojan.co")
+
+                  ],
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(left: 45.0),child:
+                  Container(
+                    height: 50,
+   width: 50,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(80),
+      color: Colors.white24
+    ),
+    child:    Icon(Icons.comment)
+                    ,
+    )
+              ),
+
+
+
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
